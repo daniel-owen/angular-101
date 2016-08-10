3931836da657cd326ca9db9ead5bb36c7be9c1d3
daniel owen <dowen35@gmail.com>
2016-08-10 16:24:48 -0400

    2nd commit

--- lab-cashier.js
+++ lab-cashier.js
@@ -1,15 +1,88 @@
-// add show and hide functionality for change results
-// add math for change calculation
+var cashRegisterApp = angular.module('cashRegisterApp', []);
+cashRegisterApp.controller('cashRegisterController', function($scope, $filter){
 
-var cashierApp = angular.module('cashierApp', []);
-cashierApp.controller('cashierController', function($scope){
+	var purse = {
+		dollar: {
+			name: "Dollar Coin",
+			value: 1.00,
+			image: "dollar.png"
+		},
+		quarter: {
+			name: "quarter",
+			value: 0.25,
+			image: "quarter-front.png"
+		},
+		dime: {
+			name: "dime",
+			value: 0.10,
+			image: "dime-front.png"
+		},
+		nickel: {
+			name: "nickel",
+			value: 0.05,
+			image: "nickle-front.png"
+		},
+		penny: {
+			name: "penny",
+			value: 0.01,
+			image: "penny-front.png"
+		}
+	};
+
+	var changeArray = [];
+	$scope.changeArray = changeArray;
 
 	$scope.makeChange = function(){
-		var change = $scope.givenAmount - $scope.billAmount;
-		$scope.quarters = 5;
-		$scope.dimes = 4;
-		$scope.nickels = 3;
-		$scope.pennies = 2;
+		// make change function called by button click (ng-click)
+		var leftOver = $filter('number')($scope.paid - $scope.total, '2');
+		// var leftOver = ($scope.paid - $scope.total) * 100;
+		$scope.totalDue = $filter('currency')(leftOver, '$', '2');
+		for(coin in purse){
+			while(purse[coin].value <= leftOver){
+				// $filter('number')(purse[coin].value, '2');
+				$scope.changeArray.push(purse[coin]);
+				leftOver -= purse[coin].value;
+				// leftOver = $filter('number')(leftOver, '2');
+			}
+		}
+
+		sortThroughCoins(changeArray);
 	}
 
+	function sortThroughCoins(arr){
+		var coinAmountArray = [];
+		var current = null;
+		var count = 0;
+		arr.sort(function(a,b){
+			if(a.value > b.value){
+				return -1;
+			}else if(a.value < b.value){
+				return 1;
+			}else{
+				return 0;
+			}
+		});
+		for(var i - 0; i < arr.length; i++){
+			if(arr[i] != current){
+				if(count > 1){
+					coinAmountArray.push("You have " + count + " " + current.name + "'s");
+				}else if(count == 1){
+					coinAmountArray.push("You have " + count + " " + current.name);
+				}
+				current = arr[i];
+				count = 1;
+			}else{
+				count++;
+			}
+		}
+		if(count > 1){
+			coinAmountArray.push("You have " + count + " " + current.name + "'s");
+		}else if(count == 1){
+			coinAmountArray.push("You have " + count + " " + current.name);
+		}
+		$scope.coinAmount = coinAmountArray;
+	}
+
+	// $scope.coinAmount
+	// $scope.changeArray
 });
---
+++ ng-me.js
@@ -0,0 +1,27 @@
+var styleApp = angular.module('styleApp', []);
+styleApp.controller('styleController', function($scope){
+	$scope.myColor = 'red';
+	
+	var Student = function(name, formerly){
+		this.name = name;
+		this.former = formerly;
+	}
+
+	$scope.students = [];
+
+	$scope.students.push(new Student('Brett', 'USAF'));
+	$scope.students.push(new Student('Wes', 'Wildcat (also Mr. Clean model)'));
+	$scope.students.push(new Student('Drew', 'Westminster Warrior'));
+	$scope.students.push(new Student('Daniel', 'Cheering for Argentina in Rio'));
+	$scope.students.push(new Student('Danielle', 'Like\'s The Who'));
+	$scope.students.push(new Student('Dan', 'Lego nerd'));
+	$scope.students.push(new Student('Danny', 'Strat Ruiner'));
+	$scope.students.push(new Student('Alex', 'Assassin. Seriously. Trust me.'));
+	$scope.students.push(new Student('Paige', 'Burnt easily'));
+	$scope.students.push(new Student('Dave', 'Cam lover'));
+	$scope.students.push(new Student('Eric', 'Useless without BS'));
+	$scope.students.push(new Student('Shirlette', 'Where the heck is she?'));
+	$scope.students.push(new Student('Jackson', 'Volition Dev.'));
+	$scope.students.push(new Student('JT', 'Every woman\'s dream'));
+	$scope.students.push(new Student('Summer', 'CS ninja'));
+});
